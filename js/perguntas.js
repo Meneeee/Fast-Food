e29b01d5c7872f09d54eb901b2e64ce64e0a333c
Meneeee <137892345+Meneeee@users.noreply.github.com>
2024-09-02 08:06:45 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,46 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com um Mac Donalds, um restaunte que prepara comida pre pronta de maneira extremamente rápida, Voce está com fome, mas sabe sobre a parte obscura desse restaurante, o que voce faz?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início, voce quis comer no Mac, mas se lembrou que eles estão envolvidos com trabalho escravo.",
+                    "Então decidiu comer a comida da sua mãe em casa."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Voce não resistiu a ilusão do Mac, teve que entrar no restaurante.",
+                    "Acabou comendo de mais e ferrou com seu estomago, além de ter apoiado o trabalho escravo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Comer no MacDonalds pode ser aterrorizante mesmo, e foi pensando nisso que um professora de saude e bem estar da escola decidiu fazer uma sequência de aulas sobre esta o tema. No fim de uma aula ela pede que você escreva um trabalho sobre o consumo de fast food. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que o fast food realmente causa problemas de saúde.",
+                
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escreve o trabalho com base em suas proprias pesquisas.",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Acabou achando os podres das redes de fast food e percebeu o quão horriveis são."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+];
